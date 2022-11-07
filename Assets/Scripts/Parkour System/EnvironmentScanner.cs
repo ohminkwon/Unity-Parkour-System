a65deec5938156b588d3ed0d9d989251036d41d6
ohminkwon <ohminkwon0720@gmail.com>
2022-11-07 19:32:03 +0900

    07-Edit Environment Scanner for detecting obstacle height

--- Assets/Scripts/Parkour System/EnvironmentScanner.cs
+++ Assets/Scripts/Parkour System/EnvironmentScanner.cs
@@ -6,6 +6,7 @@ public class EnvironmentScanner : MonoBehaviour
 {
     [SerializeField] private Vector3 forwardRayOffset = new Vector3(0, 0.25f, 0);
     [SerializeField] private float forwardRayLength = 0.8f;
+    [SerializeField] private float heightRayLength = 5;
     [SerializeField] private LayerMask obstacleLayer;
 
     public ObstacleHitData ObstacleCheck()
@@ -13,10 +14,17 @@ public class EnvironmentScanner : MonoBehaviour
         ObstacleHitData hitData = new ObstacleHitData();
 
         var forwardOrigin = transform.position + forwardRayOffset;
-        hitData.forwardHitFound = Physics.Raycast(forwardOrigin, transform.forward, 
-                                        out hitData.forwardHit, forwardRayLength, obstacleLayer);
+        hitData.forwardHitFound = Physics.Raycast(forwardOrigin, transform.forward, out hitData.forwardHit, forwardRayLength, obstacleLayer);
 
         Debug.DrawRay(forwardOrigin, transform.forward * forwardRayLength, (hitData.forwardHitFound) ? Color.red : Color.white);
+
+        if (hitData.forwardHitFound)
+        {
+            var heightOrigin = hitData.forwardHit.point + Vector3.up * heightRayLength;
+            hitData.heightHitFound = Physics.Raycast(heightOrigin, Vector3.down, out hitData.heightHit, heightRayLength, obstacleLayer);
+
+            Debug.DrawRay(heightOrigin, Vector3.down * heightRayLength, (hitData.heightHitFound) ? Color.red : Color.white);
+        }
 
         return hitData;
     }
@@ -26,4 +34,7 @@ public struct ObstacleHitData
 {
     public bool forwardHitFound;
     public RaycastHit forwardHit;
+
+    public bool heightHitFound;
+    public RaycastHit heightHit;
 }
